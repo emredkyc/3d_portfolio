d6d90678b18152f5f6441b0b12fe1fc3c49a646e
emredkyc <emredkyc@hotmail.com>
2023-12-28 11:31:11 +0100

    add assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo
+}
