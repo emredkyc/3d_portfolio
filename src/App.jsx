cdc604a8242dee6ed9411e16931f021da2f8354c
emredkyc <emredkyc@hotmail.com>
2023-12-28 02:02:59 +0100

    setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        Navbar
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+          <Route path="/projects" element={"Projects"} />
+          <Route path="contact" element={"Contact"} />
+        </Routes>
+      </Router>
+    </main>
+  )
+}
+
+export default App;
