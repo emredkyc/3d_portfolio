e1ed51538961fa1e41925ccf1e9f48df42042bab
emredkyc <emredkyc@hotmail.com>
2023-12-28 23:37:27 +0100

    Add HomeInfo component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,7 @@ import Island from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
-
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-          POPUP
-      </div> */}
+import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
@@ -51,10 +48,14 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
-      
+        <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+          {currentStage && <HomeInfo currentStage={currentStage} />}
+        </div>
+
       <Canvas
-        className={`w-full h-screen bg-transparent ${isRotating ? 
-          'cursor-grabbing' : 'cursor-grab'}`}
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
